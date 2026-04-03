30e2fe397cff92851620c48a16c14f96a3d6051d
DallasB28 <Dmbriggs1126@gmail.com>
2026-04-03 13:15:28 -0500

    Changed name of Gamerunner to TESTMAIN Changed name of modes to PersistenceManager Removed inheritance in modes Added txt file for Persistence called UserData

--- src/main/java/com/mycompany/wordle_game/ModeTwo.java
+++ src/main/java/com/mycompany/wordle_game/ModeTwo.java
@@ -7,13 +7,14 @@ import java.util.TimerTask;
  * @author dallas
  */
 public class ModeTwo {
-    private int timeout = 0;
+    private boolean timedOut = false;
     private int totalscore = 0;
     private Worker worker; 
     private Controller controller;
+    private int wins = 0;
+    private int losses = 0;
     
-    
-   public ModeTwo(Worker worker ) {
+   public ModeTwo(Worker worker, Controller controller ) {
         this.worker = worker;
         this.controller = controller;
         
@@ -22,19 +23,39 @@ public class ModeTwo {
     Timer timer = new Timer(); 
     TimerTask task1 = new TimerTask() {
         public void run() {
-            timeout=1;
-
+            timedOut = true;
+                timer.cancel();
         }
     };
-   timer.schedule(task1, 5000); // <- 5 seconds 
-   
-   
+   timer.schedule(task1, 60000); // <-  60 seconds 
    }
 
    public int getScore(){
        return totalscore;
    }
+   public boolean isTimedOut(){
+       return timedOut;
+   }
+public void updateaScores(Worker.Color[] result){
+        if (worker.allGreen(result)){
+            wins ++;
+        }
+        else if (worker.getAttempts() >= 6){
+            losses ++;
+    }
+        
+    }  //updates stuff in the game
 
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+    public void reset(){
+        wins = 0;
+        timedOut = false;
+        
+    }
 }
-
-
